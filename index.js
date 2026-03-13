bbffe9b0ba35776a4ab6115f63e9a6fd1816119c
synendra <sarpasynendra999@gmail.com>
2026-03-13 22:30:21 +0530

    implemented tasks remove feature + data persistence with local storage

--- index.js
+++ index.js
@@ -19,4 +19,23 @@ function addTask() {
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTasks() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTasks();
